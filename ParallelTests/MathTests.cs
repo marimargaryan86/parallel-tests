3c92a8fae63ceacc5c16689e31bea954e68a2c72
Mari Margaryan <mari.margaryan86@gmail.com>
2025-04-15 17:22:05 +0400

    parallel test

---
+++ ParallelTests/MathTests.cs
@@ -0,0 +1,21 @@
+namespace ParallelTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void SumTest()
+    {
+        Assert.AreEqual(6, 3 + 3);
+    }
+    
+    [Test]
+    public void MultiplyTest()
+    {
+        Assert.AreEqual(9, 3 * 3);
+    }
+}
