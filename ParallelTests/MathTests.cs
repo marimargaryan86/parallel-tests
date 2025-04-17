97450683e4424f377dd4484139c1434cf67149cd
marimargaryan86 <35734002+marimargaryan86@users.noreply.github.com>
2025-04-17 12:03:27 +0400

    Update MathTests.cs

--- ParallelTests/MathTests.cs
+++ ParallelTests/MathTests.cs
@@ -8,6 +8,13 @@ public class Tests
     }
 
     [Test]
+    public void WriteArtifactPerBatch()
+    {
+        var fileName = "my-filename-" + TestContext.CurrentContext.Test.ID + ".txt";
+        File.WriteAllText(fileName, $"This file was created in: {TestContext.CurrentContext.Test.Name}");
+        Assert.True(File.Exists(fileName));
+    }
+    [Test]
     public void SumTest()
     {
         Assert.AreEqual(6, 3 + 3);
