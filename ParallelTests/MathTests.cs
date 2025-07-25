10300678214a5fede02225d0968f306a1f0f0c31
marimargaryan86 <35734002+marimargaryan86@users.noreply.github.com>
2025-07-25 16:25:24 +0400

    Update MathTests.cs

--- ParallelTests/MathTests.cs
+++ ParallelTests/MathTests.cs
@@ -18,16 +18,6 @@ public class Tests
     {
         Assert.AreEqual(9, 3 * 3);
     }
-    [Test]
-    public void MultiplyTest_01()
-    {
-        Assert.AreEqual(9, 3 * 3);
-    }
-    [Test]
-    public void MultiplyTest_01()
-    {
-        Assert.AreEqual(9, 3 * 3);
-    }
 }
 
 public class Testsik
